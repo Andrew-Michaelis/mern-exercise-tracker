0d3210dedafbdf4875a86750b9d2383d4b503be3
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2021-11-08 16:57:09 -0700

    exercise tracker
    tutorial project using MERN standards

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditExercise extends Component {
+    render() {
+        return(
+            <div>
+                <p>on EditExercise component</p>
+            </div>
+        );
+    }
+}
